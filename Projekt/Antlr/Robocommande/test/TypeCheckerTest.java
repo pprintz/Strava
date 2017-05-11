79f00218d19bf8ad97fc978c8f2ec3336da46060
Peter Madsen <pmadse13@student.aau.dk>
2017-05-11 12:08:48 +0200

    changes to tests

--- Projekt/Antlr/Robocommande/test/TypeCheckerTest.java
+++ Projekt/Antlr/Robocommande/test/TypeCheckerTest.java
@@ -15,25 +15,33 @@ import static org.junit.Assert.assertTrue;
  */
 public class TypeCheckerTest {
     private ASTNode astBad1;
+    private ASTNode astGood1;
 
     @Before
     public void Before() throws Exception{
 
 
-          astBad1 = Main.GenerateAST(new FileInputStream("testFiles/TypeCheckerTest/bad01"));
-          astGood1 = Main.GenerateAST(new FileInputStream("testFiles/TypeCheckerTest/bad01"));
+          astBad1 = DecorateAST(Main.GenerateAST(new FileInputStream("testFiles/TypeCheckerTest/bad01")));
+          astGood1 = DecorateAST(Main.GenerateAST(new FileInputStream("testFiles/TypeCheckerTest/good01")));
+    }
+    private ASTNode DecorateAST(ASTNode ast){
+        FunctionBindingVisitor functionBindingVisitor = new FunctionBindingVisitor();
+        functionBindingVisitor.visit(ast);
+        BindingVisitor bindingVisitor = new BindingVisitor(functionBindingVisitor.getSymbolTable());
+        bindingVisitor.visit(ast);
+        return ast;
     }
     @Test
     public void TypeCheckerTest(){
         TypeChecker typeChecker = new TypeChecker();
         System.out.println("running bad1");
-        FunctionBindingVisitor functionBindingVisitor = new FunctionBindingVisitor();
-        functionBindingVisitor.visit(astBad1);
-        BindingVisitor bindingVisitor = new BindingVisitor(functionBindingVisitor.getSymbolTable());
-        bindingVisitor.visit(astBad1);
         typeChecker.visit(astBad1);
         assertFalse(typeChecker.programHasTypeErrors);
         System.out.println("... bad01 passed\n");
+        System.out.println("running good1");
+        typeChecker.visit(astGood1);
+        assertFalse(typeChecker.programHasTypeErrors);
+        System.out.println("... good1 passed\n");
     }
 
 }
